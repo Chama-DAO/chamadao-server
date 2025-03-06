09ac5890b4261c7a301ac81604d0d5696da54ef6
Dancan254 <dancanian25@gmail.com>
2025-03-06 10:55:21 +0300

    project setup

---
+++ chamadao-server/src/main/java/com/chama/chamadao_server/ChamadaoServerApplication.java
@@ -0,0 +1,13 @@
+package com.chama.chamadao_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChamadaoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChamadaoServerApplication.class, args);
+	}
+
+}
---
+++ chamadao-server/src/test/java/com/chama/chamadao_server/ChamadaoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chama.chamadao_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChamadaoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
