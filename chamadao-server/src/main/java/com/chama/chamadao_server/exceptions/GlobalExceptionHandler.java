afed20c01c6da0e9f5c8fe1382e8e3917ac52a19
Dancan254 <dancanian25@gmail.com>
2025-05-04 14:20:07 +0300

    Add GlobalExceptionHandler for centralized exception handling

---
+++ chamadao-server/src/main/java/com/chama/chamadao_server/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.chama.chamadao_server.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ErrorResponse> handleRuntimeException(RuntimeException ex) {
+        return new ResponseEntity<>(
+                new ErrorResponse(ex.getMessage()),
+                HttpStatus.BAD_REQUEST
+        );
+    }
+
+    @Data
+    @AllArgsConstructor
+    public static class ErrorResponse {
+        private String message;
+    }
+}
