d5fb91335e48a9e77e6dd3501b402c9063772e74
Dancan254 <dancanian25@gmail.com>
2025-05-05 08:28:18 +0300

    Refactor GlobalExceptionHandler to use RestControllerAdvice for improved exception handling

--- chamadao-server/src/main/java/com/chama/chamadao_server/exceptions/GlobalExceptionHandler.java
+++ chamadao-server/src/main/java/com/chama/chamadao_server/exceptions/GlobalExceptionHandler.java
@@ -4,10 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(RuntimeException.class)
