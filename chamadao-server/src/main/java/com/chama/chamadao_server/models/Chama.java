d347f024d6b0b2ebaa9a7a7ba1b86dd002efdaac
Dancan254 <dancanian25@gmail.com>
2025-05-07 20:05:47 +0300

    feat: Add Chama entity with member management and timestamps

---
+++ chamadao-server/src/main/java/com/chama/chamadao_server/models/Chama.java
@@ -0,0 +1,59 @@
+package com.chama.chamadao_server.models;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class Chama {
+    @Id
+    private String walletAddress;
+
+    private String name;
+
+    private String description;
+
+    // Creator of the Chama - commented out as per requirements
+    // @ManyToOne(fetch = FetchType.EAGER)
+    // @JoinColumn(name = "creator_wallet_address", referencedColumnName = "walletAddress")
+    // private User creator;
+
+    // List of chama members
+    @JsonManagedReference
+    @OneToMany(mappedBy = "chama", cascade = CascadeType.ALL, orphanRemoval = false)
+    private List<User> members = new ArrayList<>();
+
+    // Timestamps
+    @CreatedDate
+    private LocalDate createdAt;
+
+    @LastModifiedDate
+    private LocalDate updatedAt;
+
+    // Helper method to add a member
+    public void addMember(User user) {
+        members.add(user);
+        user.setChama(this);
+    }
+
+    // Helper method to remove a member
+    public void removeMember(User user) {
+        members.remove(user);
+        user.setChama(null);
+    }
+}
