54ea31fe14577cb79e2c85b6ce26a3206e460c05
Dancan254 <dancanian25@gmail.com>
2025-05-11 13:56:47 +0300

    feat: Add endpoint to retrieve user notifications by wallet address

--- chamadao-server/src/main/java/com/chama/chamadao_server/controllers/NotificationController.java
+++ chamadao-server/src/main/java/com/chama/chamadao_server/controllers/NotificationController.java
@@ -1,9 +1,13 @@
 package com.chama.chamadao_server.controllers;
 
+import com.chama.chamadao_server.models.Notification;
 import com.chama.chamadao_server.services.ChamaService;
 import com.chama.chamadao_server.services.NotificationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +20,10 @@ public class NotificationController {
     private final NotificationService notificationService;
     private final ChamaService chamaService;
 
-    
+    @GetMapping("/{walletAddress}")
+    ResponseEntity<List<Notification>> getUserNotifications(@PathVariable String walletAddress){
+        log.info("Request to get notifications for user {}", walletAddress);
+        List<Notification> notifications = notificationService.getUserNotifications(walletAddress);
+        return ResponseEntity.ok(notifications);
+    }
 }
