2fe63d7f05e4c9477b47b2ebd9f412b954553bdc
Dancan254 <dancanian25@gmail.com>
2025-05-13 12:05:51 +0300

    refactor: Remove unused UserRole import from ChamaService for clarity

--- chamadao-server/src/main/java/com/chama/chamadao_server/services/ChamaService.java
+++ chamadao-server/src/main/java/com/chama/chamadao_server/services/ChamaService.java
@@ -4,7 +4,6 @@ import com.chama.chamadao_server.mappers.ChamaMapper;
 import com.chama.chamadao_server.models.Chama;
 import com.chama.chamadao_server.models.User;
 import com.chama.chamadao_server.models.dto.ChamaDto;
-import com.chama.chamadao_server.models.enums.UserRole;
 import com.chama.chamadao_server.repository.ChamaRepository;
 import com.chama.chamadao_server.repository.UserRepository;
 import jakarta.persistence.EntityNotFoundException;
