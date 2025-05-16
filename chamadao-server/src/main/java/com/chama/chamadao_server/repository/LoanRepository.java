60127b7eb61645155f047c90f85ccc55f759e485
Dancan254 <dancanian25@gmail.com>
2025-05-16 16:03:18 +0300

    feat: Implement LoanRepository for managing loan data access

---
+++ chamadao-server/src/main/java/com/chama/chamadao_server/repository/LoanRepository.java
@@ -0,0 +1,26 @@
+package com.chama.chamadao_server.repository;
+
+import com.chama.chamadao_server.models.enums.LoanStatus;
+import com.chama.chamadao_server.models.loan.Loan;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface LoanRepository extends JpaRepository<Loan, Long> {
+
+    List<Loan> findByChamaAddress(String chamaAddress);
+    List<Loan> findByBorrowerWalletAddress(String walletAddress);
+    List<Loan> findByStatus(LoanStatus status);
+
+    @Query("SELECT l from Loan l LEFT JOIN FETCH l.chama LEFT JOIN FETCH l.borrower WHERE l.id = :id")
+    Optional<Loan> findWithChamaAndBorrowerById(@Param("id") Long id);
+
+    @Query("SELECT l from Loan l LEFT JOIN FETCH l.chama LEFT JOIN FETCH l.borrower WHERE l.chama.walletAddress = :chamaAddress")
+    List<Loan> findByChamaAddressWithChamaAndBorrower(@Param("chamaAddress") String chamaAddress);
+
+    @Query("SELECT l FROM Loan l LEFT JOIN FETCH l.guarantors WHERE l.id = :id")
+    Optional<Loan> findWithGuarantorsById(@Param("id") Long id);
+}
